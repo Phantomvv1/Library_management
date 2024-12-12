8510f62cfa50a6f5886acb1aa121b603c8f11c0b
Phantomvv1 <krisi_andreev@abv.bg>
2024-12-12 17:09:27 +0200

    Added all the functions to the main.go file

--- cmd/library/main.go
+++ cmd/library/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	. "github.com/Phantomvv1/Library_management/internal/books"
+	. "github.com/Phantomvv1/Library_management/internal/librarians"
 	. "github.com/Phantomvv1/Library_management/internal/users"
 	"github.com/gin-gonic/gin"
 )
@@ -8,6 +10,8 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/users", GetUsers)
+	r.GET("/books", GetBooks)
+	r.GET("/librarians", GetLibrarians)
 
 	r.Run(":42069")
 }
